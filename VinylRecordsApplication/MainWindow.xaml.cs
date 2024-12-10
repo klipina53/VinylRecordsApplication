0ab38d534ab89ff79f0fdc272ac588c0ea9bc43d
klipina53 <klipina53@gmail.com>
2024-12-11 04:39:31 +0500

    Выполненная работа на оценку 4.Липина

--- VinylRecordsApplication/MainWindow.xaml.cs
+++ VinylRecordsApplication/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +21,72 @@ namespace VinylRecordsApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public Pages.Records.Main mainRecords = new Pages.Records.Main();
+
         public MainWindow()
         {
             InitializeComponent();
+            MainWindow.mainWindow = this;
+            OpenPage(mainRecords);
         }
+
         public void OpenPage(Page pages)
         {
             frame.Navigate(pages);
         }
 
+        private void OpenRecordList(object sender, RoutedEventArgs e)
+        {
+            OpenPage(mainRecords);
+            mainRecords.LoadRecord();
+        }
+
+        private void OpenRecordAdd(object sender, RoutedEventArgs e)
+        {
+            OpenPage(new Pages.Records.Add());
+        }
+
+        private void ExportRecord(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Excel (*.xlsx)|*.xlsx";
+            saveFileDialog.RestoreDirectory = true;
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+            {
+                Classes.Record.Export(saveFileDialog.FileName, mainRecords.searchRecords);
+            }
+        }
+
+        private void OpenManufacrurersList(object sender, RoutedEventArgs e)
+        {
+            OpenPage(new Pages.Manufacturer.Main());
+        }
+
+        private void OpenManufacrurersAdd(object sender, RoutedEventArgs e)
+        {
+            OpenPage(new Pages.Manufacturer.Add());
+        }
+
+        private void OpenSupplyList(object sender, RoutedEventArgs e)
+        {
+            OpenPage(new Pages.Supple.Main());
+        }
+
+        private void OpenSupplyAdd(object sender, RoutedEventArgs e)
+        {
+            OpenPage(new Pages.Supple.Add());
+        }
+
+        private void OpenStateList(object sender, RoutedEventArgs e)
+        {
+            OpenPage(new Pages.State.Main());
+        }
+
+        private void OpenStateAdd(object sender, RoutedEventArgs e)
+        {
+            OpenPage(new Pages.State.Add());
+        }
     }
 }
