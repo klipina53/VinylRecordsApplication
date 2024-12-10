1bfbb0ec9bc084c6e7da4c0f59a1fdd0cb74f26f
klipina53 <klipina53@gmail.com>
2024-12-11 00:21:56 +0500

    Разработанный класс подключения.Lipina

---
+++ VinylRecordsApplication/Classes/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VinylRecordsApplication.Classes
+{
+    public class DBConnection
+    {
+        public static DataTable Connection(string SQL)
+        {
+            DataTable dataTable = new DataTable("Datatable");
+            SqlConnection sqlConnection = new SqlConnection("server=127.0.0.1;Trusted_Connection=No;DataBase=pr32;User=root;PWD=");
+            sqlConnection.Open();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.CommandText = SQL;
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(sqlCommand);
+            sqlDataAdapter.Fill(dataTable);
+            return dataTable;
+        }
+    }
+}
