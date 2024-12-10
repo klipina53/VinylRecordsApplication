1c12421103a4853a9e7e353b3c3fed8a05594ee1
klipina53 <klipina53@gmail.com>
2024-12-11 02:44:06 +0500

    Разработанная страница пластинок.lIPINA

--- VinylRecordsApplication/Classes/Record.cs
+++ VinylRecordsApplication/Classes/Record.cs
@@ -1,132 +1,131 @@
-﻿using System;
+﻿using OfficeOpenXml;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Media.Media3D;
 
 namespace VinylRecordsApplication.Classes
 {
     public class Record
     {
         public int Id { get; set; }
-    public string Name { get; set; }
-    public int Year { get; set; }
-    public int Format { get; set; }
-    public int Size { get; set; }
-    public int IdManufacturer { get; set; }
-    public float Price { get; set; }
-    public int IdState { get; set; }
-    public string Description { get; set; }
-    public static IEnumerable<Record> AllRecords()
-    {
-        List<Record> records = new List<Record>();
-        DataTable recordQuery = Classes.DBConnection.Connection("SELECT * FROM [dbo].[Record]");
-        foreach (DataRow row in recordQuery.Rows)
-            records.Add(new Record()
-            {
-                Id = Convert.ToInt32(row[0]),
-                Name = row[1].ToString(),
-                Year = Convert.ToInt32(row[2]),
-                Format = Convert.ToInt32(row[3]),
-                Size = Convert.ToInt32(row[4]),
-                IdManufacturer = Convert.ToInt32(row[5]),
-                Price = float.Parse(row[6].ToString()),
-                IdState = Convert.ToInt32(row[7]),
-                Description = row[8].ToString()
-            });
-        return records;
-    }
-
-    public void Save(bool Update = false)
-    {
-        string CorrectPrice = this.Price.ToString().Replace(",", ".");
-        if (Update == false)
+        public string Name { get; set; }
+        public int Year { get; set; }
+        public int Format { get; set; }
+        public int Size { get; set; }
+        public int IdManufacturer { get; set; }
+        public float Price { get; set; }
+        public int IdState { get; set; }
+        public string Description { get; set; }
+        public static IEnumerable<Record> AllRecords()
         {
-            Classes.DBConnection.Connection(
-                "INSERT INTO " +
-                    "[dbo].[Record](" +
-                    "[Name], " +
-                    "[Year], " +
-                    "[Format], " +
-                    "[Size], " +
-                    "[IdManufacturer], " +
-                    "[Price], " +
-                    "[IdState], " +
-                    "[Description]) " +
-                "VALUES(" +
-                    $"N'{this.Name}', " +
-                    $"{this.Year}, " +
-                    $"{this.Format}, " +
-                    $"{this.Size}, " +
-                    $"{this.IdManufacturer}, " +
-                    $"{CorrectPrice}, " +
-                    $"{this.IdState}, " +
-                    $"N'{this.Description}');");
-
-            this.Id = Record.AllRecords().Where(
-                x => x.Name == this.Name &&
-                x.Year == this.Year &&
-                x.Format == this.Format &&
-                x.Size == this.Size &&
-                x.IdManufacturer == this.IdManufacturer &&
-                x.IdState == this.IdState &&
-                x.Description == this.Description).First().Id;
+            List<Record> records = new List<Record>();
+            DataTable recordQuery = Classes.DBConnection.Connection("SELECT * FROM [dbo].[Record]");
+            foreach (DataRow row in recordQuery.Rows)
+                records.Add(new Record()
+                {
+                    Id = Convert.ToInt32(row[0]),
+                    Name = row[1].ToString(),
+                    Year = Convert.ToInt32(row[2]),
+                    Format = Convert.ToInt32(row[3]),
+                    Size = Convert.ToInt32(row[4]),
+                    IdManufacturer = Convert.ToInt32(row[5]),
+                    Price = float.Parse(row[6].ToString()),
+                    IdState = Convert.ToInt32(row[7]),
+                    Description = row[8].ToString()
+                });
+            return records;
         }
-        else
+
+        public void Save(bool Update = false)
         {
-            Classes.DBConnection.Connection(
-                "UPDATE [dbo].[Record] " +
-                $"SET [Name] = N'{this.Name}', " +
-                $"[Year] = {this.Year}, " +
-                $"[Format] = {this.Format}, " +
-                $"[Size] = {this.Size}, " +
-                $"[IdManufacturer] = {this.IdManufacturer}, " +
-                $"[Price] = {CorrectPrice}, " +
-                $"[IdState] = {this.IdState}, " +
-                $"[Description] = N'{this.Description}' " +
-                $"WHERE [Id] = {this.Id}");
-        }
-    }
-    public void Delete()
-    {
-        Classes.DBConnection.Connection($"DELETE FROM [dbo].[Record] WHERE [Id] = {this.Id};");
-    }
-    public static void Export(string file, List<Record> records)
-    {
-        ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
-        using (var package = new ExcelPackage())
-        {
-            ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Records");
-            worksheet.Cells[1, 1].Value = "Id";
-            worksheet.Cells[1, 2].Value = "Name";
-            worksheet.Cells[1, 3].Value = "Year";
-            worksheet.Cells[1, 4].Value = "Format";
-            worksheet.Cells[1, 5].Value = "Size";
-            worksheet.Cells[1, 6].Value = "IdManufacturer";
-            worksheet.Cells[1, 7].Value = "Price";
-            worksheet.Cells[1, 8].Value = "IdState";
-            worksheet.Cells[1, 9].Value = "Description";
-            int row = 2;
-            foreach (var record in records)
+            string CorrectPrice = this.Price.ToString().Replace(",", ".");
+            if (Update == false)
             {
-                worksheet.Cells[row, 1].Value = record.Id;
-                worksheet.Cells[row, 2].Value = record.Name;
-                worksheet.Cells[row, 3].Value = record.Year;
-                worksheet.Cells[row, 4].Value = record.Format;
-                worksheet.Cells[row, 5].Value = record.Size;
-                worksheet.Cells[row, 6].Value = record.IdManufacturer;
-                worksheet.Cells[row, 7].Value = record.Price;
-                worksheet.Cells[row, 8].Value = record.IdState;
-                worksheet.Cells[row, 9].Value = record.Description;
-                row++;
+                Classes.DBConnection.Connection(
+                    "INSERT INTO " +
+                        "[dbo].[Record](" +
+                        "[Name], " +
+                        "[Year], " +
+                        "[Format], " +
+                        "[Size], " +
+                        "[IdManufacturer], " +
+                        "[Price], " +
+                        "[IdState], " +
+                        "[Description]) " +
+                    "VALUES(" +
+                        $"N'{this.Name}', " +
+                        $"{this.Year}, " +
+                        $"{this.Format}, " +
+                        $"{this.Size}, " +
+                        $"{this.IdManufacturer}, " +
+                        $"{CorrectPrice}, " +
+                        $"{this.IdState}, " +
+                        $"N'{this.Description}');");
+
+                this.Id = Record.AllRecords().Where(
+                    x => x.Name == this.Name &&
+                    x.Year == this.Year &&
+                    x.Format == this.Format &&
+                    x.Size == this.Size &&
+                    x.IdManufacturer == this.IdManufacturer &&
+                    x.IdState == this.IdState &&
+                    x.Description == this.Description).First().Id;
             }
-            FileInfo excelP = new FileInfo(file);
-            package.SaveAs(excelP);
+            else
+            {
+                Classes.DBConnection.Connection(
+                    "UPDATE [dbo].[Record] " +
+                    $"SET [Name] = N'{this.Name}', " +
+                    $"[Year] = {this.Year}, " +
+                    $"[Format] = {this.Format}, " +
+                    $"[Size] = {this.Size}, " +
+                    $"[IdManufacturer] = {this.IdManufacturer}, " +
+                    $"[Price] = {CorrectPrice}, " +
+                    $"[IdState] = {this.IdState}, " +
+                    $"[Description] = N'{this.Description}' " +
+                    $"WHERE [Id] = {this.Id}");
+            }
+        }
+        public void Delete()
+        {
+            Classes.DBConnection.Connection($"DELETE FROM [dbo].[Record] WHERE [Id] = {this.Id};");
+        }
+        public static void Export(string file, List<Record> records)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage())
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets.Add("Records");
+                worksheet.Cells[1, 1].Value = "Id";
+                worksheet.Cells[1, 2].Value = "Name";
+                worksheet.Cells[1, 3].Value = "Year";
+                worksheet.Cells[1, 4].Value = "Format";
+                worksheet.Cells[1, 5].Value = "Size";
+                worksheet.Cells[1, 6].Value = "IdManufacturer";
+                worksheet.Cells[1, 7].Value = "Price";
+                worksheet.Cells[1, 8].Value = "IdState";
+                worksheet.Cells[1, 9].Value = "Description";
+                int row = 2;
+                foreach (var record in records)
+                {
+                    worksheet.Cells[row, 1].Value = record.Id;
+                    worksheet.Cells[row, 2].Value = record.Name;
+                    worksheet.Cells[row, 3].Value = record.Year;
+                    worksheet.Cells[row, 4].Value = record.Format;
+                    worksheet.Cells[row, 5].Value = record.Size;
+                    worksheet.Cells[row, 6].Value = record.IdManufacturer;
+                    worksheet.Cells[row, 7].Value = record.Price;
+                    worksheet.Cells[row, 8].Value = record.IdState;
+                    worksheet.Cells[row, 9].Value = record.Description;
+                    row++;
+                }
+                FileInfo excelP = new FileInfo(file);
+                package.SaveAs(excelP);
+            }
         }
     }
-}
 }
